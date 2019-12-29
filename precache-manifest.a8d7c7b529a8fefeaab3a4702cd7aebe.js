829f18589ca5123af9ef612cb49873d8393d1ca6
Harry Klein <harryklein@Harrys-MacBook-Pro.local>
2019-12-29 14:11:03 -0500

    Updates

---
+++ precache-manifest.a8d7c7b529a8fefeaab3a4702cd7aebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46c8641a815158fe5002649a285b3aa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fb95df9612b73b15364e",
+    "url": "/monsters-rolodex/static/css/main.95e0add6.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb95df9612b73b15364e",
+    "url": "/monsters-rolodex/static/js/main.2e981eae.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
